8b12dc539c2819aa2fb0c821b61391704b425c4f
Matt Fox <mfox@benchling.com>
2024-12-02 21:16:26 -0800

    Day 3

--- 3/solution.py
+++ 3/solution.py
@@ -1,23 +1,39 @@
 from typing import Generator
 
+import re
 
-FILENAME = "example_data.txt"
+FILENAME = "data.txt"
 
 
 def get_data_values() -> Generator[list[int], None, None]:
     with open(FILENAME) as file:
         data = file.readlines()
         for line in data:
-            nums = line.split()
-            yield [int(num) for num in nums]
+            yield line
 
 
 def part1() -> int:
-    pass
+    product = 0
+    for line in get_data_values():
+        match = re.findall(r"mul\((\d+),(\d+)\)", line)
+        for mul in match:
+            product += int(mul[0]) * int(mul[1])
+    return product
 
 
 def part2() -> int:
-    pass
+    product = 0
+    enabled = True
+    for line in get_data_values():
+        match = re.findall(r"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))", line)
+        for cmd, first, second in match:
+            if cmd == "don't()":
+                enabled = False
+            elif cmd == "do()":
+                enabled = True
+            elif enabled:
+                product += int(first) * int(second)
+    return product
 
 
 if __name__ == "__main__":
